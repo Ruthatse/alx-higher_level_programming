02919f1d23b70c9cd118291db2998d83f29f7770
Ruthatse <ruth.atse4@gmail.com>
2022-09-22 01:05:27 -0700

    all task

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 3-infinite_add.py
+# Ruth Atse <ruth.atse4@gmail.com>
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
+
